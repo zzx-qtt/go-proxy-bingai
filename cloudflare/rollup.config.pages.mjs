e5228404b73c98c4a539dd6522dcae764cdc8de1
jjaw <jjaw@jjaw.cn>
2024-06-15 14:06:06 +0800

    混淆

--- cloudflare/rollup.config.pages.mjs
+++ cloudflare/rollup.config.pages.mjs
@@ -17,8 +17,33 @@ export default {
 			options: {
 				// Your javascript-obfuscator options here
 				// See what's allowed: https://github.com/javascript-obfuscator/javascript-obfuscator
-                splitStrings: true,
-                splitStringsChunkLength: 5,
+                compact: true,
+                controlFlowFlattening: false,
+                deadCodeInjection: false,
+                debugProtection: false,
+                debugProtectionInterval: 0,
+                disableConsoleOutput: false,
+                identifierNamesGenerator: 'mangled',
+                log: false,
+                numbersToExpressions: false,
+                renameGlobals: false,
+                selfDefending: false,
+                simplify: true,
+                splitStrings: false,
+                stringArray: true,
+                stringArrayCallsTransform: true,
+                stringArrayCallsTransformThreshold: 0,
+                stringArrayEncoding: ['base64'],
+                stringArrayIndexShift: true,
+                stringArrayRotate: true,
+                stringArrayShuffle: true,
+                stringArrayWrappersCount: 1,
+                stringArrayWrappersChainedCalls: true,
+                stringArrayWrappersParametersMaxCount: 2,
+                stringArrayWrappersType: 'variable',
+                stringArrayThreshold: 1,
+                unicodeEscapeSequence: false,
+                target:"node"
 			},
 		})
     ]
